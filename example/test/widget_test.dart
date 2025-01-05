6d6ce6d4fb3c7ff3baec8f657df325425c6ccbe5
codelover <codelovercc@gmail.com>
2025-01-05 17:59:28 +0800

    Add `Services.fromServiceProvider` constructor test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dart_service_provider/dart_service_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_service_provider/flutter_service_provider.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -17,6 +18,33 @@ void main() {
     await tester.pumpWidget(
       Services(
         serviceConfig: (services) => services.addApplicationServices(),
+        builder: (context, _) => const MyApp(),
+      ),
+    );
+
+    expect(find.textContaining('Greetings, Singleton'), findsOneWidget);
+    expect(find.textContaining('Greetings, Transient'), findsOneWidget);
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
+
+    expect(find.textContaining('Greetings, Scope'), findsOneWidget);
+    expect(find.textContaining('Greetings, Transient'), findsOneWidget);
+    expect(find.textContaining('Greetings, Consumer'), findsOneWidget);
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
+
+    expect(find.textContaining('Greetings, New Route'), findsOneWidget);
+  });
+  testWidgets('Custom service provider test', (WidgetTester tester) async {
+    final services = ServiceCollection();
+    services.addApplicationServices();
+    final ServiceProvider rootProvider = services.buildServiceProvider();
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      Services.fromServiceProvider(
+        provider: rootProvider,
         builder: (context, _) => const MyApp(),
       ),
     );
